d20a6c64456c1d455350cf2c9c81c1413c6648c4
Paridhi Singh <paridhiraghuvanshi30@gmail.com>
2026-02-24 07:38:31 +0000

    day1changes in app component

---
+++ firstproject/src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+    <h1>HI I am Paridhi, Inside the app component</h1>
+    <h1> Second Line </h1>
+    <Employee></Employee>
+    </div>
+  
+  )
+}
+
+function Employee(){
+
+  return (
+    <h1>Employee name is Paridhi </h1>
+
+  )
+}
+export default App
