6e359558652968059f318b0a723a6fa45d66d2a7
Paridhi Singh <paridhiraghuvanshi30@gmail.com>
2026-03-10 07:32:43 +0000

    hii

--- firstproject/src/App.jsx
+++ firstproject/src/App.jsx
@@ -2,25 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Demo from './component/Demo'
 
 function App() {
  
 
   return (
     <div>
-    <h1>HI I am Paridhi, Inside the app component</h1>
-    <h1> Second Line </h1>
-    <Employee></Employee>
+     <h1>Hi i am Paridhi Singh</h1>
+     <h1>Inside the app component</h1>
+     <Employee></Employee>
+     <Demo></Demo>
     </div>
-  
   )
 }
-
 function Employee(){
-
-  return (
-    <h1>Employee name is Paridhi </h1>
-
+  return(
+    <h1>The employee name is paridhi singh</h1>
   )
 }
+
 export default App
---
+++ firstproject/src/component/Demo.jsx
@@ -0,0 +1,8 @@
+function Demo()
+{
+    return(
+        <h1>This is example of Demo </h1>
+    )
+
+}
+export default Demo
